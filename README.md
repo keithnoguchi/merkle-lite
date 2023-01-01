444db0e63980cbd1f254b2d200fdf50ed5cc3796
Keith Noguchi <keith@noguchi.us>
2023-01-01 15:23:27 -0800

    Binary Merkle Tree

---
+++ README.md
@@ -0,0 +1,5 @@
+# merkle-lite
+
+Binary Merkle Tree, designed for [Rust Crypto] hash functions.
+
+[rust crypto]: https://github.com/RustCrypto
---
+++ src/lib.rs
@@ -0,0 +1,10 @@
+//! Merkle Tree
+//!
+//! Binary Merkle Tree, designed for [Rust Crypto] hash functions.
+//!
+//! [rust crypto]: https://github.com/RustCrypto
+#![forbid(unsafe_code, missing_docs, missing_debug_implementations)]
+
+/// Merkle Tree
+#[derive(Clone, Debug)]
+pub struct MerkleTree;
